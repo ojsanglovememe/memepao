5d987c3f18dae52be80cfa716a354500e493e710
ojsanglovememe <wanglaoji1104@126.com>
2019-11-18 16:15:40 +0800

    1、调整目录

--- app/src/main/java/com/wj/babapao/ui/activitys/MainActivity.java
+++ app/src/main/java/com/wj/babapao/ui/activitys/MainActivity.java
@@ -1,8 +1,10 @@
-package com.wj.babapao;
+package com.wj.babapao.ui.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.wj.babapao.R;
 
 public class MainActivity extends AppCompatActivity {
 
